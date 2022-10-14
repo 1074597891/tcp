29cdefde30f010f12eb020c9167b879f0b10d21f
1074597891 <1074597891@qq.com>
2022-10-14 17:51:51 +0800

    tcp_client

---
+++ fy_api.py
@@ -0,0 +1,68 @@
+import hashlib
+import http
+import json
+import random
+import urllib
+import urllib.parse
+import urllib.request
+
+
+def baiduTranslate(translate_text, flag=1):
+    '''
+    :param translate_text: 待翻译的句子，len(q)<2000
+    :param flag: 1:原句子翻译成英文；0:原句子翻译成中文
+    :return: 返回翻译结果。
+    For example:
+    q=我今天好开心啊！
+    result = {'from': 'zh', 'to': 'en', 'trans_result': [{'src': '我今天好开心啊！', 'dst': "I'm so happy today!"}]}
+    '''
+
+    appid = '20221013001389483'  # 填写你的appid
+    secretKey = 'EVFxdSaIUp9GdZ2qm2wF'  # 填写你的密钥
+    httpClient = None
+    myurl = '/api/trans/vip/translate'  # 通用翻译API HTTP地址
+    fromLang = 'auto'  # 原文语种
+
+    if flag:
+        toLang = 'en'  # 译文语种
+    else:
+        toLang = 'zh'  # 译文语种
+
+    salt = random.randint(3276, 65536)
+
+    sign = appid + translate_text + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(translate_text) + '&from=' + fromLang + \
+            '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+
+    # 建立会话，返回结果
+    try:
+        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+        httpClient.request('GET', myurl)
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+        result_all = response.read().decode("utf-8")
+        result = json.loads(result_all)
+
+        # return result
+        return result['trans_result'][0]['dst']
+
+    except Exception as e:
+        print(e)
+    finally:
+        if httpClient:
+            httpClient.close()
+
+
+if __name__ == '__main__':
+    # 手动录入翻译内容，q存放
+    # q = raw_input("please input the word you want to translate:")
+    q = "介绍一下整本书，比如是传主的人生，或者作者写这本书的特色。可参看目录、序言或简介等资料。"
+    '''
+    flag=1 输入的句子翻译成英文
+    flag=0 输入的句子翻译成中文
+    '''
+    
+    result = baiduTranslate(q, flag=1)  # 百度翻译
+    print("原句:" + q)
+    print(result)
---
+++ main.py
@@ -0,0 +1,29 @@
+import socket
+
+#创建一个socket对象
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "192.168.1.3"
+port = 8888
+#绑定地址
+socket_server.bind((host, port))
+#设置监听
+socket_server.listen(5)
+# socket_server.accept()返回一个元组, 元素1为客户端的socket对象, 元素2为客户端的地址(ip地址，端口号)
+client_socket, address = socket_server.accept()
+
+#while循环是为了让对话持续
+while True:
+	# #接收客户端的请求
+    # recvmsg = client_socket.recv(1024)
+    # #把接收到的数据进行解码
+    # strData = recvmsg.decode("GBK")
+    # #设置退出条件
+    # if strData == 'q':
+    #     break
+    # print("接收: %s" % strData)
+    # #输入
+    msg = input("发送: ")
+    #发送数据，需要进行编码
+    client_socket.send(msg.encode("UTF-8"))
+#关闭服务器端
+socket_server.close()
